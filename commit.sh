e8509141812ebf4255439b6ec2eb2de40ffc63a7
slax508 <sankulaxman1998@gmail.com>
2025-05-14 04:44:03 +0100

    update

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### git commit script
+# This script is used to commit changes to a git repository.
+# It checks for uncommitted changes, prompts the user for a commit message,
+# and commits the changes with the provided message.
+# It also allows the user to specify a branch name and push the changes to that branch.
+# Usage: ./commiy.sh [branch_name]
+# Example: ./commiy.sh my-feature-branch    
+git init
+git add .
+git commit -m "update"
+git push
