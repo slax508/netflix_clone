f4e19b0458b33c9ad728410ed1143d31faf3528a
slax508 <sankulaxman1998@gmail.com>
2025-05-14 05:36:20 +0100

    update

--- commit.sh
+++ commit.sh
@@ -9,5 +9,5 @@
 git init
 git add .
 git commit -m "update"
-git remote add https://github.com/slax508/netflix_clone.git 
-git push -u origin master
+
+git push https://github.com/slax508/netflix_clone.git 
